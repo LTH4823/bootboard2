c8ed23a0a84f2ae485c3dcd4537feb4635a66d1a
LTH4823 <peter4823zx@naver.com>
2022-07-26 23:56:12 +0900

    register setting3 (controller)

--- src/main/java/org/taerock/boottest/controller/BoardController.java
+++ src/main/java/org/taerock/boottest/controller/BoardController.java
@@ -4,12 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.taerock.boottest.dto.BoardDTO;
 import org.taerock.boottest.dto.PageRequestDTO;
 import org.taerock.boottest.dto.PageResponseDTO;
 import org.taerock.boottest.service.BoardService;
+
+import javax.validation.Valid;
 
 @Controller
 @Log4j2
@@ -19,8 +24,31 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    @GetMapping("register")
+    public void registerGET() {
+
+    }
+
+    @PostMapping("register")
+    //@Valid -> 검증 annotation , Binding -> 검증 결과 확인
+    public String registerPost(@Valid BoardDTO boardDTO
+            , BindingResult bindingResult
+            , RedirectAttributes rttr) {
+
+        log.info("board register: " + boardDTO);
+
+        //hasErrors -> 결과에 대하여 어떠한 에러가 1개라도 나타난다면면
+        if (bindingResult.hasErrors()) {
+            log.info("has errors.....");
+            rttr.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+
+        return "redirect:/board/list";
+    }
+
     @GetMapping("/list")
-    public void list(PageRequestDTO pageRequestDTO, Model model){
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
 
         PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
 
@@ -28,7 +56,7 @@ public class BoardController {
         log.info(responseDTO);
         log.info("===============================================");
 
-        model.addAttribute("responseDTO" , responseDTO);
+        model.addAttribute("responseDTO", responseDTO);
 
     }
 
