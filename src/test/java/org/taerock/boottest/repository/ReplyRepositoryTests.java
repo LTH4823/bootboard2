58923672a6d6ce9bf513f190a2442ed0e3099f96
LTH4823 <peter4823zx@naver.com>
2022-08-17 00:14:46 +0900

    make reply test 3 (test code for Reply Insert)

---
+++ src/test/java/org/taerock/boottest/repository/ReplyRepositoryTests.java
@@ -0,0 +1,38 @@
+package org.taerock.boottest.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.taerock.boottest.entity.Board;
+import org.taerock.boottest.entity.Reply;
+
+@SpringBootTest
+@Log4j2
+public class ReplyRepositoryTests {
+
+    @Autowired
+    ReplyRepository replyRepository;
+
+    @Test
+    public void testInsert() {
+
+        //JPA DB에서 제일 중요한 것은 PK
+        Board board = Board.builder().bno(100).build();
+
+        for (int i = 0; i < 100; i++) {
+
+            Reply reply = Reply.builder()
+                    .board(board)
+                    .replyText(i+"번의 댓글 입니다.")
+                    .replyer("replyer00")
+                    .build();
+
+            replyRepository.save(reply);
+
+        }
+
+
+    }
+
+}
